5711651151a72b1dd0dde6779e61ecc8b242d598
Cleber <webfaria@gmail.com>
2021-06-06 09:17:14 -0300

    Update test Event Service

--- src/main/java/com/devsuperior/bds02/services/EventService.java
+++ src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.bds02.dto.EventDTO;
 import com.devsuperior.bds02.entities.City;
 import com.devsuperior.bds02.entities.Event;
+import com.devsuperior.bds02.repositories.CityRepository;
 import com.devsuperior.bds02.repositories.EventRepository;
 import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
 
@@ -18,6 +19,9 @@ public class EventService {
 	@Autowired
 	private EventRepository repository;
 
+	@Autowired
+	private CityRepository cityReopitory;
+
 	@Transactional
 	public EventDTO update(Long id, EventDTO dto) {
 		try {
@@ -25,9 +29,10 @@ public class EventService {
 			entity.setName(dto.getName());
 			entity.setDate(dto.getDate());
 			entity.setUrl(dto.getUrl());
-			
-			City city = new City();
+
+			City city = cityReopitory.getOne(entity.getCity().getId());
 			city.setId(dto.getCityId());
+
 			entity.setCity(city);
 			entity = repository.save(entity);
 			return new EventDTO(entity);
